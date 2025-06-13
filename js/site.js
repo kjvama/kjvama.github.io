d6e22b911d2b049829fb066b1ff15155336456c1
Ask me anything about the King James Bible <kjvama@gmail.com>
2025-06-14 02:06:41 +0900

    Add files via upload

--- js/site.js
+++ js/site.js
@@ -30,12 +30,12 @@ if (backbutton !== null) {
 const bookbuttons = document.querySelectorAll(".tab .book");
 
 if (bookbuttons !== null) {
-    for (var index = 0; index < bookbuttons.length; index++) {
+    for (let index = 0; index < bookbuttons.length; index++) {
         bookbuttons[index].addEventListener("click", clicked);
     }
 
     function clicked(e) {
-      location.href = this.id + ".htm";
+      location.href = "/book/" + this.id;
     }
 }
 
@@ -66,36 +66,29 @@ if (anchorbuttons !== null) {
 const topbutton = document.getElementById('btntop');
 
 if (topbutton !== null) {
+    /*
     topbutton.addEventListener('click', function () {
-        moveToTheTop();
+        window.scrollTo({ top: 0, behavior: "smooth" });
     });
+    */
+
+    topbutton.onclick = () => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    };
 
     window.addEventListener('scroll', () => {
-        if (
-          document.body.scrollTop > 100 ||
-          document.documentElement.scrollTop > 20
-        ) {
+        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 20) {
             topbutton.style.display = 'block';
         } else {
             topbutton.style.display = 'none';
         }
       });
-      
-      function moveToTheTop() {
-        const position = document.documentElement.scrollTop || document.body.scrollTop;
-        if (position) {
-          window.requestAnimationFrame(() => {
-            window.scrollTo(0, position - position / 10);
-            moveToTheTop();
-          });
-        }
-      }
 }
 
 const versebuttons = document.querySelectorAll('.contents_style button.verse');
 
 if (versebuttons !== null) {
-  for (var index = 0; index < versebuttons.length; index++) {
+  for (let index = 0; index < versebuttons.length; index++) {
     versebuttons[index].addEventListener("click", showVerse);
   }
 
@@ -122,7 +115,7 @@ if (versebuttons !== null) {
 const verseaudiobuttons = document.querySelectorAll(".contents_style button.audio");
 
 if (verseaudiobuttons !== null) {
-    for (var index = 0; index < verseaudiobuttons.length; index++) {
+    for (let index = 0; index < verseaudiobuttons.length; index++) {
       verseaudiobuttons[index].addEventListener("click", clickedOnTheAudioButton);
     }
 
@@ -137,7 +130,7 @@ const chaptertitle = document.querySelectorAll(".contents_style p.chapter");
 if (chaptertitle !== null) {
   let doctitle = document.title;
   let title = doctitle.replace("PCE - ", "");
-  for (var index = 0; index < chaptertitle.length; index++) {
+  for (let index = 0; index < chaptertitle.length; index++) {
     let booktag = document.createElement("span");
     let bookname = document.createTextNode(title);
     booktag.setAttribute("class", "bookname");
@@ -151,7 +144,7 @@ const copybuttons = document.querySelectorAll(".contents_style button.copy");
 if (copybuttons !== null) {
   let doctitle = document.title;
   let title = doctitle.replace("PCE - ", "");
-  for (var index = 0; index < copybuttons.length; index++) {
+  for (let index = 0; index < copybuttons.length; index++) {
     let btntag = copybuttons[index];
     let versebtn = btntag.parentElement.children[0];
     let versespan = btntag.parentElement.children[1];
