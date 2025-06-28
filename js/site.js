5695897e8e3ea55d94ae4dfa94b752bb567eab00
Ask me anything about the King James Bible <kjvama@gmail.com>
2025-06-28 15:10:54 +0900

    Add files via upload

--- js/site.js
+++ js/site.js
@@ -170,8 +170,8 @@ if (copybuttons !== null) {
       const copyhtml_last = copyhtml_first.replace(/<\/i>/g, "]");
       const copytext = " " + copyhtml_last;
 
-      const blobHtml = new Blob([bookverse + copyhtml_last], { type: "text/html" });
-      const blobText = new Blob([bookverse + copytext], { type: "text/plain" });
+      const blobHtml = new Blob([bookverse + " " + copyhtml_last], { type: "text/html" });
+      const blobText = new Blob([bookverse + " " + copytext], { type: "text/plain" });
 
       const data = [new ClipboardItem({
          ["text/plain"]: blobText,
